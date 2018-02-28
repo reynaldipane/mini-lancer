0553745279639dc37ec9b9fff70faeb990e7dc30
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-28 15:30:39 +0700

    Initialize some view and helpers file

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app     = express()
-const routes  = require('./routes')
-const PORT    = 3000;
+const express       = require('express')
+const app           = express()
+const routes        = require('./routes')
+const PORT          = 3000;
+const bodyParser    = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.locals.helper   = require('./helpers')
 app.use('/',routes);
 
-app.listen(PORT,() =>{
+app.listen(PORT,() => {
     console.log("application running on port 3000");
 })
