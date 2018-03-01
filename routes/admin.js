8fe674735470f936b025e35439dc50a457bb139a
agungcaproex <agung.caproex@gmail.com>
2018-03-01 13:56:00 +0700

    nambah menu admin service

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,75 @@
 const routes = require('express').Router()
+const models = require('../models')
 
 routes.get('/',(req,res) => {
     res.send('Ini halaman admin')
+})
+
+
+routes.get('/listservice',(req,res) => {
+    models.Service.findAll({
+        order: [['id','ASC']]
+      })
+      .then(services => {
+        //   res.send(services)
+        res.render('admin/listService',{services:services});
+      }).catch(err=>{
+        res.send(err)
+      });
+})
+
+
+routes.get('/addservice',(req,res) => {
+    res.render('admin/formAddService',{})
+})
+
+routes.post('/addservice',(req,res) => {
+    models.Service.create({
+        name: req.body.service_name,
+        description: req.body.description
+    }).then(services => {
+        res.redirect('/admin/listservice')
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+
+routes.get('/editservice/:id',(req,res) => {
+    models.Service.findById(req.params.id)
+    .then(services => {
+        res.render('admin/editService', {services: services})
+    })
+})
+
+routes.post('/editservice/:id',(req,res) => {
+    let objService = {
+        name: req.body.service_name,
+        description: req.body.description
+    }
+
+    models.Service.update(objService, {
+        where: {
+            id: req.params.id
+        }
+    }).then(services => {
+        res.redirect('/admin/listservice')
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+
+routes.get('/delservice/:id',(req,res) => {
+    models.Service.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(services => {
+        res.redirect('/admin/listservice')
+    }).catch(err => {
+        res.send(err)
+    })
 })
 
 module.exports = routes;
--- routes/recruiter.js
+++ routes/recruiter.js
@@ -4,4 +4,28 @@ routes.get('/',(req,res)=>{
     res.send('Ini halaman Recruiter')
 })
 
+
+routes.get('/add',(req,res) => {
+    res.send('Halaman menambahkan Recruiter')
+})
+
+routes.post('/add',(req,res) => {
+    res.send('Recruiter telah ditambahkan')
+})
+
+
+routes.get('/edit/:id',(req,res) => {
+    res.send('Halaman merubah data Recruiter')
+})
+
+routes.post('/edit/:id',(req,res) => {
+    res.send('Data Recruiter telah dirubah')
+})
+
+
+routes.get('/delete/:id', (req,res) => {
+    res.send('Hapus data Recruiter')
+})
+
+
 module.exports = routes;
