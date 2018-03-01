faa850506abd2582b3dec59fbf3e658a0892441c
agungcaproex <agung.caproex@gmail.com>
2018-03-02 02:35:15 +0700

    nambah helper, gender, semoga amaaan bang.

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,19 @@
 class Helpers {
-        
+    constructor(){
+
+    }
+    
+    static addName(gender, name){
+        if(gender == 'male'){
+            return 'Mr. '+ name
+        }
+        else if(gender == 'female'){
+            return 'Mrs. '+ name
+        }
+    }
 }
 
-module.exports = Helpers;
+
+module.exports = {
+    addName: Helpers.addName
+}
